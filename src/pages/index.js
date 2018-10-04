33b02960d3864cea074eee21b4cf68d3aa624b61
# <#>
2018-10-04 18:56:16 +0530

    Second Commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Layout from "../components/layout"
 export default () => (
     <Layout>
         <div>
-            <h1>This HOME page</h1>
+            <h1>This is HOME page</h1>
             <p>This is home page of this website</p>
             <p>Maths 1+5 = {1+5}</p>
         </div>
